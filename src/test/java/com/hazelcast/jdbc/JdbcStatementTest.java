8eab38a1fb881c329c1f7f4b77409db45a140c79
Serge Bishyr <sergii.bishyr@hazelcast.com>
2020-12-17 11:51:05 +0200

    clean up

--- src/test/java/com/hazelcast/jdbc/JdbcStatementTest.java
+++ src/test/java/com/hazelcast/jdbc/JdbcStatementTest.java
@@ -22,7 +22,6 @@ import com.hazelcast.sql.SqlStatement;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
